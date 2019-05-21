5d535bd8da5d1fa0b6ecfb88c8fa96686600a375
Joe Jamsky <cba1067950@yahoo.com>
2019-05-21 17:58:28 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,15 @@
 class Post 
   @@all = []
   
+  attr_accessor :name 
+  
   def initialize(name)
     @name = name 
-    
-    
+    @@all << name 
   end 
+  
+  def self.all
+    @@all 
+  end
   
 end 
